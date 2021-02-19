389d90f4e4c61ed7c5f5a91400f613e541480bf2
Flavissimo <Flavissimo@yandex.ru>
2021-02-19 16:58:02 +0300

    #3 create objects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
-
-//Задание №1. Функция, возвращающая случайное целое число из переданного диапазона включительно.
+const TITLING_TYPES = [
+  'Приветствие 1',
+  'Приветствие 2',
+  'Приветствие 3',
+  'Приветствие 4',
+];
+const HOUSING_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURING_TYPES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTION_TYPES =[
+  'Просторная комната',
+  'Вид из окон на море',
+  'Раздельный санузел',
+  'Большая кухня-гостинная',
+  'Удобное расположение от ресепшена',
+];
+const PHOTO_TYPES = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+//ищем случайное число
 function getRandomNumber(min, max) {
   //если max меньше min, введем переменную буфер
   if (max < min) {
@@ -16,11 +52,7 @@ function getRandomNumber(min, max) {
   // возвращаем случайное число с помощью метода Math.random и округляя число до целого
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-alert(getRandomNumber(2, 7));
-
-
-
-//Задание №2. Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+//ищем случайное число с плавающей точкой
 function getRandomQuantity(min, max, precision) { //
   if (max < min) {
     let buffer = max;
@@ -37,4 +69,66 @@ function getRandomQuantity(min, max, precision) { //
   let randomQuantity = min + ((Math.floor(((Math.random() * (max - min + 1))) * 10 ** precision)) / (10 ** precision));
   return randomQuantity;
 }
-alert(getRandomQuantity(2.2, 5, 4));
+//массив строк — массив случайной длины из значений
+let shuffle = (array) => { //взято: https://learn.javascript.ru/task/shuffle
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    [array[i], array[j]] = [array[j], array[i]];//перемешали элементы массива
+  }
+
+  return array;
+};
+//случайное число длины
+const PHOTO_AMOUNT = 10;//длина
+let perem = getRandomNumber(1, PHOTO_AMOUNT);
+//console.log(perem);
+
+let myNewFunction = (array1, size) => {
+  const PHOTOS = [];//новый массив, куда записываем случайный элемент
+  for (let i = 0; i < size; i++) {
+    let element = array1[0, getRandomNumber(0, array1.length-1)];
+    PHOTOS.push(element);
+  }
+  return PHOTOS;
+};
+//координаты в плавающей точкой
+let getLocation = () =>{
+  let location = {
+    x: getRandomQuantity(35.6500, 35.7000, 5),
+    y: getRandomQuantity(139.7000, 139.8000, 5),
+  };
+  return location;
+};
+//Общий объект
+let createHotel = () => {
+  let newArray = shuffle(FEATURING_TYPES);//новый сгенерированный массив
+  newArray = newArray.slice(0, getRandomNumber(1, newArray.length-1));//метод slice
+  //console.log(newArray);
+  let PHOTOS = myNewFunction(PHOTO_TYPES, perem);
+  //перезаписываем координаты в новую переменную
+  let location2 = getLocation();
+  return  {
+    author:{//первый объект
+      avatar: `img/avatars/user0${getRandomNumber(1,8)}.png`,
+    },
+    offer:{//второй объект
+      title: TITLING_TYPES[getRandomNumber(0,TITLING_TYPES.length -1)],
+      address: (`${location2.x}, ${location2.y}`),//интерполяция
+      price: getRandomNumber(1, 1000000),//Любое положительное число
+      types: HOUSING_TYPES[getRandomNumber(0,HOUSING_TYPES.length-1)],
+      rooms: getRandomNumber(1, 150),//Любое положительное число
+      guests: getRandomNumber(1, 300),//Любое положительное число
+      checkin:TIMES[getRandomNumber(0,TIMES.length-1)],
+      checkout:TIMES[getRandomNumber(0,TIMES.length-1)],
+      featurs: newArray,//массив строк — массив случайной длины из значений
+      description: DESCRIPTION_TYPES[getRandomNumber(0,DESCRIPTION_TYPES.length-1)],
+      photos: PHOTOS,//массив строк — массив случайной длины из значений
+    },
+    location:{
+      x: getRandomQuantity(35.6500, 35.7000, 5),
+      y: getRandomQuantity(139.7000, 139.8000, 5),
+    },
+  };
+};
+createHotel();
+//console.log(createHotel());

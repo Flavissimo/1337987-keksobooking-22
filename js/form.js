f1a67de5421e2645cc1e3b9077f8e2f7b46dfeca
Flavissimo <Flavissimo@yandex.ru>
2021-03-15 14:05:03 +0300

    #6 fixing the code

--- js/form.js
+++ js/form.js
@@ -1,41 +1,40 @@
+const PRICE_TYPE = {//заведем внешний глобальный объект
+  bungalow: {
+    placeholder: '0',
+    min: 0,
+  },
+  flat: {
+    placeholder: '1 000',
+    min: 1000,
+  },
+  house: {
+    placeholder: '5 000',
+    min: 5000,
+  },
+  palace: {
+    placeholder: '10 000',
+    min: 10000,
+  },
+}
+
+
 //форма
 const form = document.querySelector('.ad-form');
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-});
-
 //типы жилья, вывести выбранный тип
 const typeOfDwelling  = form.querySelector('#type');
-
-typeOfDwelling.addEventListener('change', (event) => {
-  const minPrice = document.querySelector('#price');//находим цену
-  for (let item of event.target.children) {//выбирает ребенка в цикле
-    if (item.selected) {
-      const selectedItem = item.value;//выбранный тип жилья
-      if (selectedItem === 'bungalow') {
-        minPrice.placeholder = '0';
-        minPrice.min = 0;
-      }
-      if (selectedItem === 'flat') {
-        minPrice.placeholder = '1 000';
-        minPrice.min = 1000;
-
-      }
-      if (selectedItem === 'house') {
-        minPrice.placeholder = '5 000';
-        minPrice.min = 5000;
-      }
-      if (selectedItem === 'palace') {
-        minPrice.placeholder = '10 000';
-        minPrice.min = 10000;
-      }
-      break;
-    }
-  }
-});
 //синхронное время
 const checkIn = form.querySelector('#timein');
 const checkOut = form.querySelector('#timeout');
+
+const typeofDwellingHandler = (evt) => {
+  const minPrice = document.querySelector('#price');//находим цену
+  const selectedItem = evt.target.value; //означает выделенный параметр selected (выбранный тип жилья)
+  const setting = PRICE_TYPE[selectedItem];//нашли соответствующие параметры в объекте
+  if (setting) { //проверка, действительно ли мы параметры нашли
+    minPrice.placeholder = setting.placeholder;//установили их
+    minPrice.min = setting.min;
+  }
+};
 
 const onChangeOfTime = (evt) => {
   const itemTime = evt.target.value;
@@ -43,20 +42,9 @@ const onChangeOfTime = (evt) => {
   checkOut.value = itemTime;
 };
 
-checkIn.addEventListener('change', (evt) => { onChangeOfTime(evt); });
-checkOut.addEventListener('change', (evt) => { onChangeOfTime(evt); });
-
-// СТАРАЯ ВЕРСИЯ
-// const getTimeInOut = () => {
-//   document.querySelector('#timein').addEventListener('change', (event) => {
-//     const timeOut = document.querySelector('#timeout');
-//     for ( let timeIn of event.target.children){
-//       if (timeIn.selected) {
-//         const selectedTimeIn = timeIn.value;
-//         timeOut.value = selectedTimeIn;
-//         break;
-//       }
-//     }
-//   });
-// }
-// getTimeInOut();
+checkIn.addEventListener('change', onChangeOfTime);//установка слушателей вконце!
+checkOut.addEventListener('change', onChangeOfTime);
+typeOfDwelling.addEventListener('change', typeofDwellingHandler);
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+});
